7c6d46083ee7848fd2d000f5c189024944837083
mayds1010 <mayds1010@gmail.com>
2022-12-22 12:29:18 +0900

    commit

--- src/main/java/com/yojulab/toy_servlets/dao/PollWithDB.java
+++ src/main/java/com/yojulab/toy_servlets/dao/PollWithDB.java
@@ -8,21 +8,24 @@ import java.sql.SQLException;
 
 
 public class PollWithDB {
-public void getQuestion(String questionUid) throws SQLException{
-  
+    public HashMap<String, Object> getQuestion(String questionsUid) throws SQLException{
+
         Commons commons = new Commons();
-        Statement statement = commons.getStatement(); 
-  
-        String query = "SELECT * FROM QUESTIONS_LIST" +    
-                         "WHERE QUESTIONS_UID = '"+questionUid+"'";              
-   
+        Statement statement = commons.getStatement();
+
+        String query = "SELECT * FROM QUESTIONS_LIST " +
+                        " WHERE QUESTIONS_UID = '"+questionsUid+"'";
+        
         ResultSet resultSet = statement.executeQuery(query);
-        HashMap<String,Object> result;
-        while(resultSet.next());    
-         resultSet.getString("QUESTIONS_UID");
-         resultSet.getString("QUESTIONS");
-         resultSet.getString("ORDERS");
-         
+        HashMap<String, Object> result = null;
+        while(resultSet.next()){
+            result = new HashMap<>();
+            result.put("QUESTIONS_UID",resultSet.getString("QUESTIONS_UID"));
+            result.put("QUESTIONS", resultSet.getString("QUESTIONS"));
+            result.put("ORDERS",resultSet.getInt("ORDERS"));
+        }
+
+        return result;
     }
 }
 
--- src/main/java/com/yojulab/toy_servlets/servlets/DetailServlets.java
+++ src/main/java/com/yojulab/toy_servlets/servlets/DetailServlets.java
@@ -2,26 +2,41 @@ package com.yojulab.toy_servlets.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.HashMap;
 
+import com.yojulab.toy_servlets.dao.PollWithDB;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = " /poll/PollServlet")
+@WebServlet(urlPatterns = "/polls/PollServlet")
 public class DetailServlets extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      //input type
-       String questions_Uid =  request.getParameter("QUESTIONS_UID"); //밖에서 가져올때
-       
-      //biz with DB and class
-      
-       //output with html
-       response.setContentType("text/html;charset=UTF-8");
-       PrintWriter printWriter = response.getWriter();
-       printWriter.close();
+        // input type
+        String questions_Uid = request.getParameter("QUESTIONS_UID");
+
+        // biz with DB and Class
+        PollWithDB pollWithDB = new PollWithDB();
+        HashMap<String, Object> question = null;
+        try {
+            question = pollWithDB.getQuestion(questions_Uid);
+            System.out.println(question.get("QUESTIONS_UID"));
+            System.out.println(question.get("QUESTIONS"));
+            System.out.println(question.get("ORDERS"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // output with html
+        request.setAttribute("question", question);
+        
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/polls/details.jsp");
+        requestDispatcher.forward(request, response);
     }
 }
 
