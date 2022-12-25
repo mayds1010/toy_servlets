7cf53705b1defd22d0d2d8ace1547752c86ee3ab
mayds1010 <mayds1010@gmail.com>
2022-12-25 21:33:10 +0900

    commit

--- src/main/java/com/yojulab/toy_servlets/dao/PollWithDB.java
+++ src/main/java/com/yojulab/toy_servlets/dao/PollWithDB.java
@@ -7,17 +7,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class PollWithDB {
- 
-    public HashMap<String, Object> getQuestion(String questions_Uid) throws SQLException{
+   
+    public HashMap<String, Object> getQuestion(String questionsUid) throws SQLException{
 
         Commons commons = new Commons();
         Statement statement = commons.getStatement();
-
-        String query = "SELECT * FROM QUESTIONS_LIST " +
-                        " WHERE QUESTIONS_UID = '"+questions_Uid+"'";
         
-        ResultSet resultSet = statement.executeQuery(query);
+        String query = "SELECT * FROM QUESTIONS_LIST " +
+                        " WHERE QUESTIONS_UID = '" + questionsUid + "'";
+       
+         ResultSet resultSet = statement.executeQuery(query);
+        
         HashMap<String, Object> result = null;
+        
         while(resultSet.next()){
             result = new HashMap<>();
             result.put("QUESTIONS_UID",resultSet.getString("QUESTIONS_UID"));
@@ -27,30 +29,33 @@ public class PollWithDB {
 
         return result;
     }
-    public ArrayList<String> getExample(ArrayList<String> questions_Uid, Object answer) throws SQLException{ //추가
-        Commons commons = new Commons();
-        Statement statement = commons.getStatement();
-
-        String query = "SELECT * FROM ANSWERS INNER JOIN EXAMPLE_LIST " +
-        " ON ANSWERS.EXAMPLE_UID = EXAMPLE_LIST.EXAMPLE_UID " +
-       " WHERE QUESTIONS_UID = '"+questions_Uid+"'";
-       
-        new ArrayList<>();
-        return null;
+    public ArrayList<HashMap> getAnswer(String questions_Uid) throws SQLException { 
     
+        Commons commons = new Commons();
+    Statement answerStatement = commons.getStatement();
+
+    String query = "SELECT EXAMPLE_LIST.EXAMPLE_UID, EXAMPLE_LIST.ORDERS, EXAMPLE_LIST.EXAMPLE " +
+                    "FROM ANSWERS" + "INNER JOIN EXAMPLE_LIST" 
+                    + "ON ANSWERS.EXAMPLE_UID = EXAMPLE_LIST.EXAMPLE_UID" 
+                    + " WHERE QUESTIONS_UID = '" + questions_Uid + "' " + "ORDER BY ORDERS";
+
+    ResultSet resultSetAnswers = answerStatement.executeQuery(query);
+
+    ArrayList<HashMap> answers = new ArrayList<HashMap>();
+    HashMap<String, Object> example_list = null;
+
+    while (resultSetAnswers.next()) {
+      example_list = new HashMap<>();
+      example_list.put("ORDERS", resultSetAnswers.getInt("ORDERS"));
+      example_list.put("EXAMPLE", resultSetAnswers.getString("EXAMPLE"));
+      example_list.put("EXAMPLE_UID", resultSetAnswers.getString("EXAMPLE_UID"));
+
+      answers.add(example_list);
     }
+    return answers;
+  }
     public ArrayList<String> getQuestion_Uid() {
-        return null;
-    }
-    public ArrayList<String> getExample(String questions_Uid) {
-        return null;
-    }
-    public ArrayList<HashMap> getExamples(String questions_Uid) {
-        return null;
+      return null;
     }
 }
 
-
-
-// SELECT * FROM QUESTIONS_LIST
-    // WHERE QUESTIONS_UID = 'Q2'
